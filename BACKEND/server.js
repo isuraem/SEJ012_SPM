aa226f5c6944349f166e8054fc537d0037504109
isuraem <erandaim@gmail.com>
2022-08-21 14:03:25 +0530

    structer

---
+++ BACKEND/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(express.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+
+const connection = mongoose.connection;
+//connect database
+ connection.once("open",() => {
+    console.log("mongodb Connection Sucess !");
+ })
+
+
+ 
+ app.listen(PORT, () => {
+    console.log(`server is up and running on porT: ${PORT}`);
+})
