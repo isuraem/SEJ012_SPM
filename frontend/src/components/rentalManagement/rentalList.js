c19c13d78761550ac0d75bf16a85c6d76dadace7
OshadhaGeeth <oshadaclans@gmail.com>
2022-10-12 18:18:54 +0530

    add delete modal

--- frontend/src/components/rentalManagement/rentalList.js
+++ frontend/src/components/rentalManagement/rentalList.js
@@ -4,6 +4,7 @@ import { Link, useHistory } from "react-router-dom";
 import { Modal } from "react-bootstrap";
 
 import Header from "../Header";
+import DeleteModal from "./modals/deleteRental";
 
 function RentalList() {
 
@@ -12,6 +13,9 @@ function RentalList() {
     
     const [rentalList, setRentalList] = useState([]);
    
+    const [modalDataDelete, setModalDataDelete] = useState([]);
+    const [modalDeleteConfirm, setModalDeleteConfirm] = useState(false);
+    const [modalDelete, setModalDelete] = useState(false);
    
 
     useEffect(() => {
@@ -27,11 +31,19 @@ function RentalList() {
                 })
             }
             getRentals();
+    }, [])
 
-        }, [])
+    useEffect(() => {
 
-  
+        console.log("component did update", modalDataDelete)
+    
+    
+    }, [modalDataDelete]);
 
+    const openModalDelete = (data) => {
+        setModalDataDelete(data);
+        setModalDeleteConfirm(true);
+    }
 
 
     function refreshPage() {
@@ -54,11 +66,7 @@ function RentalList() {
                             + Add Rental
                         </button>
                     </a>
-                    {/* <p class="float-right ml-4">
-                        <button class="btn btn-ok white" id="pending" onClick={pendingRecords}>
-                            Pending Rental
-                        </button>
-                    </p> */}
+                    
                 </div>
                 <div class="row table-head-search">
                     <div className="col-md-8"></div>
@@ -82,6 +90,7 @@ function RentalList() {
                             <th>Customer Name</th>
                             <th>Total (Rs.)</th>
                             <th>Status</th>
+                            <th>Action</th>
                            
                         </tr>
                     </thead>
@@ -98,6 +107,19 @@ function RentalList() {
                                     <td >{rental.customerName}</td>
                                     <td >{rental.finalPrice.toFixed(2)}</td>
                                     <td >{rental.status}</td>
+                                    <td>
+
+                                        <button
+                                            class="btn btn-light btn-sm"
+                                            //onClick={() => openModalUpdate(rental)}
+                                        >
+                                            update
+                                        </button>
+                                        <Link class="btn btn-danger btn-sm" 
+                                        onClick={() => openModalDelete(rental)}
+                                        role="button"> remove</Link>
+
+                                    </td>
                                    
                                 </tr>
                             );
@@ -105,6 +127,45 @@ function RentalList() {
                     </tbody>
                 </table>
             </div>
+
+            <Modal show={modalDeleteConfirm} size="md"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm Deletion</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Are you want to delete this item ?</p>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <div className="row">
+                        <div className="col -6">
+                            <button type="submit" className="btn btn-delete" onClick={() => { setModalDelete(true); setModalDeleteConfirm(false); }}>
+                                Confirm
+                            </button>
+                        </div>
+                        <div className="col-6 text-right" onClick={() => setModalDeleteConfirm(false)}>
+                            <button type="reset" className="btn btn-reset">
+                                cancel
+                            </button>
+                        </div>
+                    </div>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal
+                show={modalDelete}
+                onHide={() => setModalDelete(false)}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <DeleteModal
+                    data={modalDataDelete}
+                    onHide={() => setModalDelete(false)}
+                />
+            </Modal>
 
 
         </div >
