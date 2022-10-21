cede3beccea586043c9e509bc8ef3a9a9b843b9d
LalithyaMaddumage <sandeepanilalithya155@gmail.com>
2022-10-21 18:42:28 +0530

    router file update

--- frontend/src/homes.js
+++ frontend/src/homes.js
@@ -6,9 +6,16 @@ import AddVehicle from './components/vehicleManagement/vehicleAdd';
 import vehicleList from './components/vehicleManagement/vehicleList';
 import DeletedList from './components/vehicleManagement/deletedList';
 import VehicleReport from './components/vehicleManagement/VehicleReport';
+
+import Event from "./components/eventReservationManagement/Event";
+
+import ViewEvent from "./components/eventReservationManagement/viewEvent";
+
+import EventReport from "./components/eventReservationManagement/EventReport";
+import login from './components/login'
 import Footer from './Footer';
 
-export const Home = () => {
+export const homes = () => {
   return (
     <div>
         <Router>
@@ -17,6 +24,26 @@ export const Home = () => {
             <Route path = "/vehicle/viewVehicle" exact component={vehicleList}/>
             <Route path = "/vehicle/view" exact component={DeletedList}/>
             <Route path = "/vehicle/viewVehicleReport" exact component={VehicleReport}/>
+            <Route path = "/" exact component={login}/>
+
+            <Route path="/addEvent" exact>
+
+            <Event />
+
+            </Route>
+
+            <Route path="/viewEvent">
+
+            <ViewEvent />
+
+            </Route>
+
+            <Route path="/reservation/report">
+
+            <EventReport />
+
+            </Route>
+            
             </Switch>
             <Footer></Footer>
         </Router>
